88a4fc33008e9311384a38d39607acd7dfe7714b
Alena <Alena@DESKTOP-PA11SPG>
2022-04-11 21:14:37 +0300

    Добавьте файлы проекта.

---
+++ Lesson 2 GroupPlugin/CopyGroup.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_GroupPlugin
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class CopyGroup : IExternalCommand
+    {
+        private object groupPickFilter;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UiDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            Reference reference = uidoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберите группу элементов");
+        }
+    }
+}
